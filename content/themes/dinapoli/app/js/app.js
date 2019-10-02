d1b9e2262923edd2a04575d3197d7a8da5be4bbd
sckyzopat <mariannexpiquet@gmail.com>
2019-10-02 13:10:31 +0200

    responsive mobile + tablet ok - remains desktop responsive

--- content/themes/dinapoli/app/js/app.js
+++ content/themes/dinapoli/app/js/app.js
@@ -2,7 +2,29 @@ var app = {
 
     init: function() {
         console.log('pattern scss');
+
+        $('a[href^="#"]').on('click', app.scrollSection);
+    },
+
+
+    scrollSection: function(event){
+        event.preventDefault();
+
+        var target = $(this).attr('href');
+
+       /* le sélecteur $(html, body) permet de corriger un bug sur chrome
+       et safari (webkit) */
+        $('html, body')
+
+       // on arrête toutes les animations en cours
+       .stop()
+
+       /* on fait maintenant l'animation vers le haut (scrollTop) vers
+        notre ancre target */
+       .animate({scrollTop: $(target).offset().top}, 1500 );
+
     }
+
 }
 
 $(app.init);
